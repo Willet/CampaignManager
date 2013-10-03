80ef47c9349e79f19fd5fc57aa6311ebc0d73521
Alex Riedler <alex@riedler.ca>
2013-10-03 13:00:04 -0700

    fast fail out on setup script.

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@
 export PATH=node_modules/.bin:${PATH}
 
 echo "Installing SASS and COMPASS"
-gem install sass compass || echo "NOTE: This scripts requires NPM, and Ruby installed"
+gem install sass compass || echo "FAILED: verify you have latest version of NPM and Ruby installed" && exit
 
 echo "Installing Required Node Packages for DEV environment (package.json)"
-npm install
+npm install || echo "FAIL: Couldn't install node packages, try to get 'npm install' to work" && exit
 
 echo "Installing Yeoman"
-npm install yo || echo "NOTE: This scripts requires NPM, and Ruby installed"
+npm install yo || echo "FAIL: Couldn't install yeoman, try to get 'npm install yo' to work" && exit
 
 echo "Grabbing remote packages (bower.json)"
-bower install
+bower install || echo "FAIL: Could not succesfully grab css/js packages. 'bower install'" && exit
 
 echo ""
 echo "'grunt server' to run the server locally with live reload etc!"
