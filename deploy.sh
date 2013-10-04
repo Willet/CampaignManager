97a7435579d0bd85f180197cb80eae0dd7027b46
Brian Lai <b2lai@uwaterloo.ca>
2013-10-04 16:42:12 -0400

    add deployment shell script

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# sudo apt-get install s3cmd
+# s3cmd --configure
+# New settings:
+#   Access Key: ...
+#   Secret Key: ...
+#   Encryption password: do not enter one
+#   Path to GPG program: /usr/bin/gpg
+#   Use HTTPS protocol: False
+#   HTTP Proxy server name:
+#   HTTP Proxy server port: 0
+
+print_msg () {
+    local BLUE="\033[1;36m";
+    local NO_COLOUR="\033[0m";
+    echo $BLUE $1 $NO_COLOUR;
+}
+
+# require parameter 1
+if [ "$1" = "" ]; then
+    print_msg "Usage: $0 <bucket name, e.g. campaign-manager-test>" && exit 1
+fi
+
+# require s3cmd to be configured
+if [ ! -f ~/.s3cfg ]; then
+    print_msg "Configure s3cmd first using: s3cmd --configure" && exit 1
+fi
+
+print_msg  "Building project..."
+
+grunt build || (print_msg "Build failed" && exit 1)
+
+print_msg  "Uploading project..."
+
+s3cmd sync ./ s3://$1.secondfunnel.com/ || (print_msg "Sync failed" && exit 1)
+
+print_msg  "Correcting project permissions..."
+
+s3cmd setacl --acl-public --recursive s3://$1.secondfunnel.com/ || (print_msg "Permission thingy failed" && exit 1)
+
+print_msg  "Done!"
