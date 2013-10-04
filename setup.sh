b41834415dcc4c1ca215005a62733f3b3f43d55a
Brian L <b2lai@uwaterloo.ca>
2013-10-04 16:38:52 -0400

    Fix operator precedence exiting script even if code succeeds

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@
 export PATH=node_modules/.bin:${PATH}
 
 echo "Installing SASS and COMPASS"
-gem install sass compass || echo "FAILED: verify you have latest version of NPM and Ruby installed" && exit
+gem install sass compass || (echo "FAILED: verify you have latest version of NPM and Ruby installed" && exit)
 
 echo "Installing Required Node Packages for DEV environment (package.json)"
-npm install || echo "FAIL: Couldn't install node packages, try to get 'npm install' to work" && exit
+npm install || (echo "FAIL: Couldn't install node packages, try to get 'npm install' to work" && exit)
 
 echo "Installing Yeoman"
-npm install -g yo grunt bower || echo "FAIL: Couldn't install yeoman, try to get 'npm install yo' to work" && exit
+npm install -g yo grunt bower || (echo "FAIL: Couldn't install yeoman, try to get 'npm install yo' to work" && exit)
 
 echo "Grabbing remote packages (bower.json)"
-bower install || echo "FAIL: Could not succesfully grab css/js packages. 'bower install'" && exit
+bower install || (echo "FAIL: Could not succesfully grab css/js packages. 'bower install'" && exit)
 
 echo ""
 echo "'grunt server' to run the server locally with live reload etc!"
