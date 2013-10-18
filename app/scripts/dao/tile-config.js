a3233d5c99762ab48bebc64a60d7fefcdb4a09c3
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2013-10-17 22:54:37 -0400

    Basic add / remove tile config on accept / reject
    - I don't always do things in one commit, but when I do, its the night before a deadline and I'm at work way too late.

---
+++ app/scripts/dao/tile-config.js
@@ -0,0 +1,86 @@
+define([
+    "app", "dao/base", "entities"
+], function (App, Base, Entities) {
+    var API = {
+        getFromConfig: function(config) {
+            var id, field, template;
+            config = config || {};
+
+            switch(config.template) {
+                case 'product':
+                    field = 'product-ids';
+                    break;
+                default:
+                    field = 'content-ids';
+            }
+
+            id = config.id || -1;
+            template = config.template || 'image';
+
+            return {
+                'id': id,
+                'field': field,
+                'template': template
+            }
+        },
+        addTileConfig: function(page_id, config) {
+            var tileConfigCollection, obj = {};
+
+            config = this.getFromConfig(config);
+
+            obj['template'] = config.template;
+            obj[config.field] = [config.id];
+
+            tileConfigCollection = new Entities.TileConfigCollection();
+            tileConfigCollection.url = App.API_ROOT + "/page/" + page_id + "/tile-config";
+            tileConfigCollection.create(obj)
+            //tileConfig.save();
+        },
+
+        deleteTileConfig: function(page_id, config) {
+            var tileConfigCollection, obj = {};
+
+            config = this.getFromConfig(config);
+
+            obj[config.field] = [config.id];
+
+            tileConfigCollection = new Entities.TileConfigCollection();
+            tileConfigCollection.url = App.API_ROOT + "/page/" + page_id + "/tile-config";
+
+            tileConfigCollection.fetch({
+                data: obj
+            }).done(function() {
+                //http://stackoverflow.com/questions/10858935/cleanest-way-to-destroy-every-model-in-a-collection-in-backbone
+                var i;
+                for (i = tileConfigCollection.length -1; i >= 0; i--) {
+                    tileConfigCollection.at(i).destroy();
+                }
+            });
+        }
+    }
+
+    App.reqres.setHandler("tileconfig:approve", function(page_id, config) {
+        API.addTileConfig(page_id, config);
+    });
+
+    App.reqres.setHandler("tileconfig:reject", function(page_id, config) {
+        API.deleteTileConfig(page_id, config);
+    });
+
+//    var API = {
+//        login: function (username, password) {
+//            var user = new Entities.User();
+//            user.url = App.API_ROOT + "/user"; //
+//            user.login(username, password);
+//            return user;
+//        },
+//        logout: function () {
+//
+//        }
+//    };
+//
+//    App.reqres.setHandler("user:login", function (username, password) {
+//        var user = API.login(username, password);
+//        App.user = user;
+//    });
+});
