b4e6e59a5d28d4f3fe45061c346e49ed49857ce9
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2013-11-13 16:24:03 -0500

    Fixed bug caused by upgrade from 1.0.0 to 1.1.0
    - More details on the solution, and why [here](https://github.com/jashkenas/backbone/issues/2458)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -94,6 +94,19 @@ require.config({
     }
 });
 
+// Patch change from Backbone 1.0.0 to 1.1.0
+require(['backbone'], function(Backbone) {
+    'use strict';
+    var View = Backbone.View;
+    Backbone.View = View.extend({
+        constructor: function (options) {
+            this.options = options;
+            View.apply(this, arguments);
+        }
+    });
+    return Backbone;
+});
+
 require([
     'app',
     'jquery',
