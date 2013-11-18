4d74831623a2574e40fa0babd2146edeadd09ffd
Kevin Simpson <ktsimpso@gmail.com>
2013-11-18 17:59:53 -0500

    shim to allow window.location.origin in ie. This is required for the local proxy to work.

---
+++ app/scripts/config/ie.js
@@ -0,0 +1,8 @@
+require([], function () {
+    'use strict';
+
+    //Shim to have window.location.origin in ie
+    if (!window.location.origin) {
+        window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+    }
+});
