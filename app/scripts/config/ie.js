37f6dd9e480ba43a09e0736009f6a60b100abdd1
Kevin Simpson <ktsimpso@gmail.com>
2013-11-19 11:08:42 -0500

    Changed quote to pass lint

--- app/scripts/config/ie.js
+++ app/scripts/config/ie.js
@@ -3,6 +3,6 @@ require([], function () {
 
     //Shim to have window.location.origin in ie
     if (!window.location.origin) {
-        window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+        window.location.origin = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
     }
 });
